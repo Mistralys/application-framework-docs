0dad70b63db94b8a508e7fa4400439f18b10421a
Mistralys <s.mordziol@gmail.com>
2023-09-18 11:12:07 +0200

    Added the web UI handler.

---
+++ src/DocumentationHub.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package App Framework Documentation
+ * @see \Mistralys\AppFrameworkDocs\DocumentationHub
+ */
+
+declare(strict_types=1);
+
+namespace Mistralys\AppFrameworkDocs;
+
+use Mistralys\MarkdownViewer\DocsConfig;
+use Mistralys\MarkdownViewer\DocsManager;
+use Mistralys\MarkdownViewer\DocsViewer;
+
+/**
+ * Main documentation class: Handles the necessary Markdown Viewer
+ * configuration to render the documents.
+ *
+ * @package App Framework Documentation
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
+class DocumentationHub
+{
+    private DocsManager $manager;
+    private string $vendorURL;
+
+    public function __construct(string $vendorFolder, string $vendorURL)
+    {
+        $this->vendorURL = $vendorURL;
+
+        $config = (new DocsConfig());
+            //->addIncludePath($vendorFolder . '/communication/builder-content-generator/templates')
+            //->addIncludeExtension('tpl');
+
+        $this->manager = (new DocsManager($config))
+            ->addFolder(__DIR__ . '/../documents', true);
+    }
+
+    public static function create(string $vendorFolder, string $vendorURL): DocumentationHub
+    {
+        return new DocumentationHub($vendorFolder, $vendorURL);
+    }
+
+    public function display(): void
+    {
+        (new DocsViewer($this->manager, $this->vendorURL))
+            ->setTitle('Application Framework Documentation')
+            ->display();
+    }
+}
